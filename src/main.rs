8cbae0044dd46ae45e8475d4a421a7df6cab15d6
Aman Sinha <aksinha.101010@gmail.com>
2021-09-08 21:47:41 +0530

    implementation for sending request body

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
 mod cli;
 mod metrics;
 mod request;
-use metrics::{ResponseType, show_metrics};
+use metrics::{show_metrics, ResponseType};
+use std::collections::HashMap;
+use std::fs::read_to_string;
+use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
 use std::thread;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Arc::new(cli::Cli::get_arguments());
     let metrics = Arc::new(Mutex::new(Vec::<ResponseType>::new()));
+    let json_string = Arc::new(check_json_file(&args.body)?);
 
     let mut thread_handler: Vec<std::thread::JoinHandle<()>> = Vec::new();
     for i in 0..args.clients {
         let args = args.clone();
         let metrics = metrics.clone();
+        let json = json_string.clone();
         thread_handler.push(
             thread::Builder::new()
                 .name(String::from(format!("Client {}", i)))
                 .spawn(move || {
-                    request::create_request(&args, &metrics).unwrap();
+                    request::create_request(&args, &metrics, &json).unwrap();
                 })
                 .unwrap(),
         );
@@ -28,4 +33,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     show_metrics(metrics);
     Ok(())
+}
+
+fn check_json_file(
+    body: &Option<PathBuf>,
+) -> Result<Option<HashMap<String, serde_json::Value>>, Box<dyn std::error::Error>> {
+    match body {
+        Some(value) => {
+            let json_string = read_to_string(value)?;
+            let map: HashMap<String, serde_json::Value> = serde_json::from_str(&json_string)?;
+            Ok(Some(map))
+        }
+        None => Ok(None),
+    }
 }
--- src/request.rs
+++ src/request.rs
@@ -1,36 +1,43 @@
 use crate::cli::Cli;
 use crate::metrics::ResponseType;
-use reqwest::blocking::Client;
-use reqwest::blocking::RequestBuilder;
-use reqwest::Url;
+use reqwest::{blocking::Client, blocking::RequestBuilder, Url};
+use std::collections::HashMap;
 use std::sync::Mutex;
 use std::time::Instant;
 
 pub fn create_request(
     args: &Cli,
     metrics: &Mutex<Vec<ResponseType>>,
+    json: &Option<HashMap<String, serde_json::Value>>,
 ) -> Result<(), Box<dyn std::error::Error>> {
     match args.number {
         Some(n) => {
             for _ in 0..n {
-                send_request(args, metrics)?;
+                send_request(args, metrics, json)?;
             }
         }
         None => {
             let request_timer = Instant::now();
             let arg_time = args.time.unwrap();
             while request_timer.elapsed().as_secs_f64() < arg_time {
-                send_request(args, metrics)?;
+                send_request(args, metrics, json)?;
             }
         }
     }
     Ok(())
 }
 
-fn send_request(args: &Cli, metrics: &Mutex<Vec<ResponseType>>) -> Result<(), Box<dyn std::error::Error>> {
+fn send_request(
+    args: &Cli,
+    metrics: &Mutex<Vec<ResponseType>>,
+    json: &Option<HashMap<String, serde_json::Value>>,
+) -> Result<(), Box<dyn std::error::Error>> {
     for path in &args.paths {
         let url = Url::parse(format!("{}://{}", args.scheme, args.host).as_str())?.join(&path)?;
-        let req = request_builder(&args.method, url);
+        let mut req = request_builder(&args.method, url);
+        if let Some(value) = json {
+            req = req.header("Content-Type", "application/json").json(&value);
+        }
         let timer = Instant::now();
         let res = req.send();
         let response_time = timer.elapsed().as_millis();
